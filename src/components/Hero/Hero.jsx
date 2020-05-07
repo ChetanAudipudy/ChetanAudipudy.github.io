66f386645ffbca47e11d30fabc69cb45511f50ac
ChetanAudipudy <chetan.audipudy@gmail.com>
2020-05-07 13:58:18 -0500

    star fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-scroll";
 
 const Hero = () => (
+    <React.Fragment>
     <div className="hero">
         <div className="hero__container">
             <h3 className="hero__h3">Hello there! I'm</h3>
@@ -10,16 +11,18 @@ const Hero = () => (
             <h3 className="hero__h3 hero__h3--anim-delay">I'm a Software Developer</h3>
         </div>
         <Link 
-                to="skillset" 
-                spy={true} 
-                smooth={true}
-                offset={-50}
-                duration={500}
-                className="arrow"></Link>
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
+            to="skillset" 
+            spy={true} 
+            smooth={true}
+            offset={-50}
+            duration={500}
+            className="arrow">
+        </Link>
     </div>
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+    </React.Fragment>
 )
 
 export default Hero
