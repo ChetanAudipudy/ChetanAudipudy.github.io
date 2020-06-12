5907a5bea7171f54eb8d96c07c92b71e9d0a86f5
ChetanAudipudy <chetan.audipudy@gmail.com>
2020-06-11 19:32:41 -0500

    resume button, skillset fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,10 +10,14 @@ const Contact = () => (
                 <h4 className="contact__h4">Get in touch with me:</h4>
                 <div className="contact__items__container">
                     <div className="contact__items"><GrMail className="react-icons"/> chetan.audipudy@gmail.com</div>
+                    <div className="contact__items_icons">
                     <a href="https://github.com/ChetanAudipudy" target="_blank" rel="noopener 
                     noreferrer" className="contact__link"><GrGithub className="react-icons"/></a>
                     <a href="https://www.linkedin.com/in/chetan-audipudy/" target="_blank" rel="noopener 
                     noreferrer" className="contact__link"><GrLinkedinOption className="react-icons"/></a>
+                    <a href="https://drive.google.com/file/d/1vJt9_4NYLvcY_CtRQbRvMYuF3no6TOph/view?usp=sharing" target="_blank" rel="noopener 
+                    noreferrer" className="contact__link"><button className="contact__button">Resume</button></a>
+                    </div>
                 </div>
             </div>
         </div>
