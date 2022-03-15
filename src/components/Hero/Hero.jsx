dbec7974c5826dc23d2e6e1df5e3d39bf7587e03
Chetan Audipudy <chetan.audipudy@gmail.com>
2022-03-14 19:44:57 -0500

    update hero background

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,11 +5,12 @@ const Hero = () => (
     <React.Fragment>
     <div className="hero">
         <div className="hero__container">
-            <h3 className="hero__h3">Hello there! I'm</h3>
-            <h1 className="hero__h1">Chetan Audipudy</h1>
-            <div className="hero__line"></div>
-            <h3 className="hero__h3 hero__h3--anim-delay">I'm a Software Engineer</h3>
-        </div>
+            
+                <h3 className="hero__h3">Hello there! I'm</h3>
+                <h1 className="hero__h1">Chetan Audipudy</h1>
+                <div className="hero__line"></div>
+                <h3 className="hero__h3 hero__h3--anim-delay">I'm a Software Engineer</h3>
+            </div>
         <Link 
             to="skillset" 
             spy={true} 
